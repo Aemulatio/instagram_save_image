a6fcb74b2a15f434e778b9c76754ff87b9e484b6
Stepan S <dumn2011@gmail.com>
2022-05-17 15:20:57 +0900

    additional check for tagName, delete logs

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-console.log("script outer message")
 let url, fileName;
 
 function saveImage(event) {
-		let div = event.target;
-		console.log(div);
-		console.log(div.attributes)
-		if ("role" in div.attributes) {
-			return;
-		} else {
-			console.log(event)
+	let div = event.target;
+	if ("role" in div.attributes) {
+		return;
+	} else {
+		if (div.tagName.toLowerCase() === "div") {
 			url = div.previousElementSibling.firstElementChild.getAttribute('src');
 			fileName = url.match(/(\/\w*.((jpg)|(png)))/)[0].replace("/", "");
 		}
+	}
 }
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 	sendResponse({url: url, fileName: fileName});
+	url = undefined;
+	fileName = undefined;
 });
 document.addEventListener("contextmenu", function (event) {
 	saveImage(event)
