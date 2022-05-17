f1d3296a409aecb681efc320d7fb4f0c1cac7c73
Stepan S <dumn2011@gmail.com>
2022-05-17 15:06:26 +0900

    save injected to content

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
-// chrome.runtime.onMessage.addListener(
-// 	function (request, sender, sendResponse) {
-// 		console.log(request)
-// 		console.log(sender)
-// 		console.log("in script")
-// 		return true;
-// 	}
-// );
 console.log("script outer message")
+let url, fileName, isSave = false, event_target;
 
+function saveImage(event) {
+	// if (isSave) {
+		console.log(event)
+		let div = event.target;
+		console.log(div);
+		console.log(div.attributes)
+		if ("role" in div.attributes) {
+			return;
+		} else {
+			console.log(event)
+			url = div.previousElementSibling.firstElementChild.getAttribute('src');
+			fileName = url.match(/(\/\w*.((jpg)|(png)))/)[0].replace("/", "");
+			// saveAs(url, fileName)
+		}
+	// }
+}
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    // console.log(request, sender, sendResponse);
-    sendResponse('我收到你的消息了：' + JSON.stringify("request"));
-    // console.log(request)
-    // console.log(sender)
-    // console.log(sendResponse)
+
+	console.log(request, sender, sendResponse);
+
+	// if (request.menuItemId === 'save') {
+	// 	isSave = true
+	// } else {
+	// 	isSave = false
+	// }
+
+	// sendResponse('我收到你的消息了：' + JSON.stringify("request"));
+	sendResponse({url: url, fileName: fileName, target: event_target});
+	// console.log(request)
+	// console.log(sender)
+	// console.log(sendResponse)
 });
-
 document.addEventListener("contextmenu", function (event) {
-    let div = event.target;
-    console.log(div);
-    console.log(typeof div)
-    console.log(div.classList)
-    console.log(div.attributes)
-    if ("role" in div.attributes) {
-        console.log("role")
-    } else {
-        const url = div.previousElementSibling.firstElementChild.getAttribute('src'),
-            fileName = url.match(/(\/[\w]*.((jpg)|(png)))/);
-        saveAs(url, fileName[0].replace("/", ""))
-    }
-
-    // console.log(event)
-    // console.log(document.querySelector(event.target).previousSibling)
+	saveImage(event)
+	event_target = event;
 });
